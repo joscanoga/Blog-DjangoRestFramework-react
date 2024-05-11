c6acc922c16ff83b264574a94fca63f5f5c68d12
joscanoga <51725572+joscanoga@users.noreply.github.com>
2024-05-11 18:00:30 -0500

    Desde cero

--- app/apps/category/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Category
-
-class CategorySerializer(serializers.ModelSerializer):
-    thumbnail = serializers.CharField(source='get_thumbnail')
-    class Meta:
-        model = Category
-        fields = ['id',  'name', 'thumbnail']
--- app/apps/category/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('categories', ListCategoriesView.as_view(), name="categories-all"),
-]
