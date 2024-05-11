b7bd90c617fba3d2dde7289604d7db9871a99c76
joscanoga <51725572+joscanoga@users.noreply.github.com>
2024-05-11 18:03:03 -0500

    desde cero

--- app/apps/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('', BlogListView.as_view(), name='blog_list'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
-]
--- app/src/redux/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import blog from './blog';
-import categories from './categories';
-export default combineReducers({blog,categories})
